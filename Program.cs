20e5e62d33ed15dbb69f4a5e5ca76c5e285faf8b
anna <annagridchina@mail.ru>
2022-12-27 15:42:07 +0700

    решение

--- Program.cs
+++ Program.cs
@@ -3,14 +3,17 @@
 //452 -> 11
 //82 -> 10
 //9012 -> 12
-//Строки использовать нельззя
+//Строки использовать нельзя
 
 Console.WriteLine("Start");
-
-
-
-
-
-
-
+int n = 9012;
+int Sum = 0;
+int i = 0;
+while (n > 0)
+{
+    i = n % 10;
+    Sum = Sum + i;  
+    n = n / 10;
+} 
+    Console.WriteLine($"сумма цифр: {Sum}");
 Console.WriteLine("END");
